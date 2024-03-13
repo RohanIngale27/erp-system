d33dbfee87a78f7e3b6bc04d1ecd1e33d30d1184
Rohan Ingale <rohaningale01@gmail.com>
2024-03-13 23:44:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import ProductsPage from './pages/ProductsPage';
 import OrdersPage from './pages/OrdersPage'; // Import OrdersPage
 import CalendarPage from './pages/CalendarPage';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
+  return (  
+    <div className="App">
+      <BrowserRouter basename="/erp-system">
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
+          <Route exact path="/erp-system" element={<DashboardPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/orders" element={<OrdersPage />} /> {/* Updated route for OrdersPage */}
           <Route path="/calendar" element={<CalendarPage />} />
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>      
+    </div>
   );
 }
 
